e6e35a3babc4dde649d934c6a5793cca2b72c28e
chenziyi <c-zy22@mails.tsinghua.edu.cn>
2024-07-05 15:59:39 +0800

    chore: clean code

--- src/source/Data_Mem.v
+++ src/source/Data_Mem.v
@@ -65,38 +65,40 @@ module Data_Mem (
 		// for (i = 8; i < RAM_SIZE; i = i + 1)
 		//     RAM_Data[i] <= 32'h00000000;
 
-			// insert sort and binary insert sort test case
 
-			RAM_Data[0] <= 32'h00000014;
-			RAM_Data[1] <= 32'h000041a8;
-			RAM_Data[2] <= 32'h00003af2;
-			RAM_Data[3] <= 32'h0000acda;
-			RAM_Data[4] <= 32'h00000c2b;
-			RAM_Data[5] <= 32'h0000b783;
-			RAM_Data[6] <= 32'h0000dac9;
-			RAM_Data[7] <= 32'h00008ed9;
-			RAM_Data[8] <= 32'h000009ff;
-			RAM_Data[9] <= 32'h00002f44;
-			RAM_Data[10] <= 32'h0000044e;
-			RAM_Data[11] <= 32'h00009899;
-			RAM_Data[12] <= 32'h00003c56;
-			RAM_Data[13] <= 32'h0000128d;
-			RAM_Data[14] <= 32'h0000dbe3;
-			RAM_Data[15] <= 32'h0000d4b4;
-			RAM_Data[16] <= 32'h00003748;
-			RAM_Data[17] <= 32'h00003918;
-			RAM_Data[18] <= 32'h00004112;
-			RAM_Data[19] <= 32'h0000c399;
-			RAM_Data[20] <= 32'h00004955;
-			
-			for (i = 21; i < RAM_SIZE; i = i + 1)
-				RAM_Data[i] <= 32'h00000000;
-				
-//		// original test case
+		// insert sort and binary insert sort test case
+
+		RAM_Data[0] <= 32'h00000014;
+		RAM_Data[1] <= 32'h000041a8;
+		RAM_Data[2] <= 32'h00003af2;
+		RAM_Data[3] <= 32'h0000acda;
+		RAM_Data[4] <= 32'h00000c2b;
+		RAM_Data[5] <= 32'h0000b783;
+		RAM_Data[6] <= 32'h0000dac9;
+		RAM_Data[7] <= 32'h00008ed9;
+		RAM_Data[8] <= 32'h000009ff;
+		RAM_Data[9] <= 32'h00002f44;
+		RAM_Data[10] <= 32'h0000044e;
+		RAM_Data[11] <= 32'h00009899;
+		RAM_Data[12] <= 32'h00003c56;
+		RAM_Data[13] <= 32'h0000128d;
+		RAM_Data[14] <= 32'h0000dbe3;
+		RAM_Data[15] <= 32'h0000d4b4;
+		RAM_Data[16] <= 32'h00003748;
+		RAM_Data[17] <= 32'h00003918;
+		RAM_Data[18] <= 32'h00004112;
+		RAM_Data[19] <= 32'h0000c399;
+		RAM_Data[20] <= 32'h00004955;
 		
-//		for (i = 0; i < RAM_SIZE; i = i + 1) begin
-//		   RAM_Data[i] <= 32'h00000000;
-//		end
+		for (i = 21; i < RAM_SIZE; i = i + 1)
+			RAM_Data[i] <= 32'h00000000;
+
+
+		// // original test case
+		
+		// for (i = 0; i < RAM_SIZE; i = i + 1) begin
+		//    RAM_Data[i] <= 32'h00000000;
+		// end
 
 	end
 
@@ -118,6 +120,7 @@ module Data_Mem (
             
             // for (i = 8; i < RAM_SIZE; i = i + 1)
             //     RAM_Data[i] <= 32'h00000000;
+
 
             // insert sort and binary insert sort test case
 
@@ -145,12 +148,13 @@ module Data_Mem (
             
             for (i = 21; i < RAM_SIZE; i = i + 1)
                 RAM_Data[i] <= 32'h00000000;
-                
-//        // original test case
-        
-//        for (i = 0; i < RAM_SIZE; i = i + 1) begin
-//           RAM_Data[i] <= 32'h00000000;
-//        end
+
+
+			// // original test case
+				
+			// for (i = 0; i < RAM_SIZE; i = i + 1) begin
+			// 	RAM_Data[i] <= 32'h00000000;
+			// end
 
 		end
 		// write data to RAM
